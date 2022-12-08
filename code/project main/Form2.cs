9add8df7d3766117b77f93dac1d214331fc04ec5
upasana khatiwada <upashanakhatiwada1234@gmail.com>
2022-12-08 18:02:30 +0545

    login and confirmation edit

--- code/project main/Form2.cs
+++ code/project main/Form2.cs
@@ -13,6 +13,9 @@ namespace project_main
 {
     public partial class Form2 : Form
     {
+        //SqlCommand cmd  ;
+        //SqlDataReader dr;
+        //SqlConnection con;
         SqlConnection con = new SqlConnection(
           @"Data Source= .\SQLEXPRESS; 
             Initial Catalog= admin;
@@ -50,49 +53,147 @@ namespace project_main
 
         private void Form2_Load(object sender, EventArgs e)
         {
+            //con= new SqlConnection(
+            //  @"Data Source= .\SQLEXPRESS; 
+            //    Initial Catalog= admin;
+            //    user id =sa ; 
+            //    password =kist@123;");
+            con.Open();
         }
 
         private void button1_Click(object sender, EventArgs e)
         {
-            if(textBox1.Text == "" || textBox2.Text=="")
-            {
-               MessageBox.Show("please enter details");
+            //    if (textBox1.Text == "" || textBox2.Text == "")
+            //    {
+            //        MessageBox.Show("please enter details");
 
-            }
-            else
-            {
-            try
-            {
-                //to save the data of textbox into database
-                con.Open();
-                string query = "Insert into employees1" + "( name,phone_number,password)" +
-                    "values('" +
-                    textBox1.Text +
-                    "','" + textBox2.Text +
-                    "','" + textBox3.Text +
-                    //"','" + textBox4.Text +
-                    "')";
-                SqlCommand cmd = con.CreateCommand();
-                cmd.CommandText = query;
-                cmd.ExecuteNonQuery();
-                MessageBox.Show("Your account has been created successfully");
-                con.Close();
-                //to go to login page 
-                this.Hide(); //hides the first form 
-                Form1 LoginForm = new Form1();
-                LoginForm.ShowDialog();
+            //    }
+            //    //else
+            //    //{
+            //    //try
+            //    //{
+            //    //to save the data of textbox into database
+            //   // con.Open();
+            //            string query = "Insert into employees1" + "( name,phone_number,password)" +
+            //                            "values('" +
+            //                             textBox1.Text +
+            //                            "','" + textBox2.Text +
+            //                            "','" + textBox3.Text +
+            //                 //"','" + textBox4.Text +
+            //                        "')";
+            //                SqlCommand cmd = con.CreateCommand();
+            //                cmd.CommandText = query;
+            //                cmd.ExecuteNonQuery();
 
-            }
-            catch (Exception ex)
-            {
-                MessageBox.Show("error");
 
+            //    if (textBox2.Text== textBox3.Text)
+            //    {
+            //        MessageBox.Show("Your account has been created successfully");
+            //        Form1 LoginForm = new Form1();
+            //        LoginForm.ShowDialog();
+
+            //    }
+            //    else
+            //    {
+            //        MessageBox.Show("error");
+
+            //    }
+            //    con.Close();
+            //    this.Hide(); //hides the first form 
+            //   // Form1 LoginForm = new Form1();
+            //    //LoginForm.ShowDialog();
+
+
+            //    //}
+            //    //catch (Exception ex)
+            //    //{
+            //    //    MessageBox.Show("error");
+
+
+            //    //}
+
+            if (textBox1.Text != string.Empty || textBox2.Text != string.Empty || textBox3.Text != string.Empty)
+            {
+                if (textBox2.Text == textBox3.Text)
+                {
+                    SqlCommand cmd = new SqlCommand("select * from employees1 where username='" + textBox1.Text + "'", con);
+                    SqlDataReader dr = cmd.ExecuteReader();
+                    if (dr.Read())
+                    {
+                        dr.Close();
+                        MessageBox.Show("Username Already exist please try another ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                    }
+                    else
+                    {
+                        dr.Close();
+                        //cmd = new SqlCommand("insert into LoginTable values(@username,@password)", con);
+                        //cmd.Parameters.AddWithValue("username", textBox1.Text);
+                        //cmd.Parameters.AddWithValue("password", textBox2.Text);
+                        //cmd.ExecuteNonQuery();
+
+
+                        string query = "Insert into employees1" + "( name,phone_number,password)" +
+                                                   "values('" +
+                                                    textBox1.Text +
+                                                   "','" + textBox2.Text +
+                                                   "','" + textBox3.Text +
+                                               //"','" + textBox4.Text +
+                                               "')";
+                        cmd = con.CreateCommand();
+                        cmd.CommandText = query;
+                        cmd.ExecuteNonQuery();
+                        MessageBox.Show("Your Account is created . Please login now.", "Done");
+                    }
+
+                }
 
             }
 
         }
+
     }
-    }
+
+
+
     
     
 }
+
+
+
+
+
+
+
+
+//if (txtconfirmpassword.Text != string.Empty || txtpassword.Text != string.Empty || txtusername.Text != string.Empty)
+//{
+//    if (txtpassword.Text == txtconfirmpassword.Text)
+//    {
+//        cmd = new SqlCommand("select * from LoginTable where username='" + txtusername.Text + "'", cn);
+//        dr = cmd.ExecuteReader();
+//        if (dr.Read())
+//        {
+//            dr.Close();
+//            MessageBox.Show("Username Already exist please try another ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+//        }
+//        else
+//        {
+//            dr.Close();
+//            cmd = new SqlCommand("insert into LoginTable values(@username,@password)", cn);
+//            cmd.Parameters.AddWithValue("username", txtusername.Text);
+//            cmd.Parameters.AddWithValue("password", txtpassword.Text);
+//            cmd.ExecuteNonQuery();
+//            MessageBox.Show("Your Account is created . Please login now.", "Done", MessageBoxButtons.OK, MessageBoxIcon.Information);
+//        }
+//    }
+//    else
+//    {
+//        MessageBox.Show("Please enter both password same ", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+//    }
+//}
+//else
+//{
+//    MessageBox.Show("Please enter value in all field.", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+//}
+//}
